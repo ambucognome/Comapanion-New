44ae5f75994db9d535f04c86ee8a046fff0fb687
ambucognome <106067154+ambucognome@users.noreply.github.com>
2023-03-20 12:23:13 +0530

    Create ReelNotificationViewController.swift
    reel notification controller

---
+++ Companion/Companion/ReelNotificationViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ReelNotificationViewController.swift
+//  Companion
+//
+//  Created by ambu sangoli on 3/17/23.
+//
+
+import UIKit
+
+class ReelNotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        title = "Notifications"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
